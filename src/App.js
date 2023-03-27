14a57bb476fc2fc6f2388588bfe7a7a6a426246f
Dylan <dylanhockersmith@gmail.com>
2023-03-26 20:47:48 -0400

    fixing dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
 import { Cart } from "./pages/cart/cart";
-import { ShopContext, ShopContextProvider } from "./context/shop-context";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
